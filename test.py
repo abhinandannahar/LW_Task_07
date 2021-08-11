853744885e61ddb0b28afc66eea910eb2375fb0e
abhinandannahar <abhinandannahar378@gmail.com>
2021-08-11 14:26:28 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import cgi
+import subprocess
+
+print("content-type: text/html")
+print()
+
+a = cgi.FieldStorage()
+cmd = a.getvalue("s")
+output = subprocess.getoutput("sudo "+cmd)
+print(output)
